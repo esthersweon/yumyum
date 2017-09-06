5401f19879476f85160a8739aa50d1d679bd9ba0
raybchoi <raybchoi@gmail.com>
2017-09-05 22:28:28 -0700

    able to send back all truck data and reviews data for one truck

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -4,9 +4,20 @@
 
 const db = require('../models');
 
+function reviewsDataForOneTruck(req, res) {
+  console.log(req.params.truckId)
+  console.log('Review Results is getting data')
+  db.Review.find({foodTruck: req.params.truckId}, function(err, allReviewsData) {
+    if (err) {
+      console.log('reviewsDataForTruck has an error: ', err)
+    }
+    res.json(allReviewsData);
+    console.log('ReviewsData sent back: ', allReviewsData);
+  });
+};
 
 module.exports = {
-  // searchResultsPage: searchResultsPage,
+  reviewsDataForOneTruck: reviewsDataForOneTruck,
   // createNewProfile: createNewProfile,
   // showOneProfile: showOneProfile,
   // updateOneProfile: updateOneProfile,
