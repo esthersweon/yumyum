fa09159d15408cfe8429a9604c0dd06cc89633de
raybchoi <raybchoi@gmail.com>
2017-09-07 10:46:42 -0700

    CRD on reviews

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -8,16 +8,34 @@ function reviewsDataForOneTruck(req, res) {
   console.log(req.params.truckId)
   console.log('Review Results is getting data')
   db.Review.find({foodTruck: req.params.truckId}, function(err, allReviewsData) {
+    let arrayOfReviewsToBeShown = [];
+    let arrayOfReviewsMarkedForDeletion = [];
+    allReviewsData.forEach( function(reviewData) {
+      if ( reviewData.markedForDeletion === false ) {
+        arrayOfReviewsToBeShown.push(reviewData)
+      } else {
+        arrayOfReviewsMarkedForDeletion.push(reviewData);
+      }
+    });
     if (err) {
       console.log('reviewsDataForTruck has an error: ', err)
     }
-    res.json(allReviewsData);
-    console.log('ReviewsData sent back: ', allReviewsData);
+    // can i somehow send back the name of the foodtruck with the review?
+    res.json(arrayOfReviewsToBeShown);
+    console.log('ReviewsData sent back: ', arrayOfReviewsToBeShown);
   });
 };
 
 function createReview(req, res) {
   console.log('createReview route is working');
+  console.log(req.body)
+  db.Review.create(req.body, function(err, newReview) {
+    if (err) {
+      console.log('ERROR ON CREATE', err)
+    }
+    res.json(newReview);
+    console.log('NEW PROFILE INFO SENT BACK', newReview)
+  })
 };
 
 function editReview(req, res) {
@@ -26,6 +44,16 @@ function editReview(req, res) {
 
 function deleteReview(req, res) {
   console.log('deleteReview route is working');
+  console.log('THIS IS THE REVIEW PARAM',req.params.truckId )
+  db.Review.findByIdAndUpdate(req.params.truckId, {$set: {
+    markedForDeletion: req.body.markedForDeletion
+  }}, {new: true}, function(err, removedReview) {
+      if (err) {
+        console.log ('THERE WAS AN ERROR DURING deleteReview ', err);
+      }
+      console.log('deleteReview SAVED AND JSON SENT BACK ');
+      res.json(removedReview);
+    });
 };
 
 module.exports = {
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,10 @@
 console.log('app.js is still linked')
 $(document).ready(function(){
 
-  // //initialize all modals           
+  // //initialize all modals
   //  $('.modal').modal();
    $('.modal').modal();
-      
+
 
 
   $.ajax({
@@ -30,8 +30,8 @@ $(document).ready(function(){
 
 // $('#modal1').on('click',  function () {
 // 	console.log('model has been clicked')
-	
-   
+
+
 // })
 // the document is referring to the actual html page. saying once the HTML page is loaded ilisten for the click on the 'modal-triggers' and then open
 // this is to edit the truck
@@ -45,6 +45,13 @@ $(document).on("click", ".save-truck-edit", SaveTruck);
 
 $(document).on('click', '.delete-truck', removeTruck);
 
+$(document).on('click', '.read-truck-reviews', renderAllReview);
+
+$(document).on('click', '.delete-review', deleteReview);
+
+$(document).on('click', '.write-truck-review', writeReviewInputOpen);
+
+$(document).on('click', '.write-truck-review-submit', writeReviewSubmit);
 
 function renderAllTrucks (trucks) {
 	trucks.forEach(function (truck) {
@@ -66,14 +73,14 @@ function SaveTruck(e) {
     })
     .then(function(savedTruckData) {
     	console.log('DATA IS HERE', savedTruckData);
-    	
+
     	$('[data-truck-id =' + truckId + ']').remove();
 
     	renderTruck(savedTruckData);
 
     	$('[data-truck-id =' + truckId + ']')[0].scrollIntoView();
 
-    	
+
     })
 
 }
@@ -86,7 +93,7 @@ function editTruck() {
 
 	var editTruckHtml = `
 
-		<section id='truck-form' class="container"> 
+		<section id='truck-form' class="container">
   <div class="row">
     <form id="form" class="col s12">
                                 <!-- Truck Name  -->
@@ -182,11 +189,11 @@ function renderTruck(truck) {
 	console.log('Rending the damn trucks', truck)
 
 var trucksHTML = (`
-	 	 
+
 	 	  <div class="card card-truck" data-truck-id="${truck._id}">
             <div class="card-image waves-effect waves-block waves-light">
               <img class="activator" src="${truck.logo}">
-              
+
             </div>
             <div class="card-content">
             <span class="card-title activator grey-text text-darken-4">${truck.typesOfFood}<i class="material-icons right"></i></span>
@@ -203,6 +210,8 @@ var trucksHTML = (`
 				<!-- Modal Trigger -->
   				<a class="waves-effect waves-light btn modal-triggers" data-truck='${JSON.stringify(truck)}'>Edit Truck</a>
               <button class='btn btn-danger red delete-truck'>Delete Truck</button>
+              <button class='btn btn-danger yellow read-truck-reviews review-buttons'>Read Reviews</button>
+              <button class='btn btn-danger blue write-truck-review review-buttons'>Write A Review</button>
             </div>
           </div>`);
 
@@ -210,18 +219,18 @@ var trucksHTML = (`
 
 
 $('#trucks').prepend(trucksHTML)
- 		
+
 };
 
 
 
-   
+   // end of document.ready
 });
 
 
 var trucksHTMLForm = function (truckData) { `
-	
-`} 
+
+`}
 
 function removeTruck () {
 
@@ -237,7 +246,7 @@ var removeTruckData = {
 $.ajax ({
     method: 'delete',
     url: '/api/' + truckId,
-   	data: removeTruckData, 
+   	data: removeTruckData,
   })
 .then(function (truckRemoved) {
 	console.log('This truck has been removed', truckRemoved);
@@ -264,3 +273,206 @@ $.ajax ({
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function renderAllReview (reviews) {
+
+  console.log('read reviews button is working');
+  let currentTruckId
+  currentTruckId = $(this).closest('.card-truck').attr('data-truck-id');
+  console.log('gotId', currentTruckId)
+  $.ajax({
+  method: 'GET',
+  url: '/api/' + currentTruckId + '/reviews'
+  })
+  .then(function(currentProfileData) {
+    $('.review-information').fadeOut(300, function() {
+      $(this).remove();
+    });
+    console.log('THIS IS THE renderAllReview data coming back', currentProfileData);
+    currentProfileData.forEach(function (review) {
+      renderReview(review);
+    });
+  })
+  .catch(function(err) {
+    console.log('renderAllReview failed ', err)
+  });
+
+
+
+}
+
+
+// render one review
+function renderReview(review) {
+	console.log('Rendering one single review', review);
+
+  // goes through the array of review.image and then creates a new array with the img src tag. then it joins the imge together into one string
+  let imgHTML = review.image.map(x => `<img src='${x}'/>`).join('');
+
+var reviewHTML = (`
+    <div class="review-information" data-review-id='${review._id}'>
+      <div class="row reviews">
+        <div class="col s12 review-information-info">
+          <div>
+            <h2 class='reviews' name='reviews' value=''></h2>
+          </div>
+          <div>
+            <div>
+              <span class='reviews-atmosphere reviews' name='atmosphere' value=''>atmosphere: ${review.atmosphere}</span>
+            </div>
+            <div>
+              <span class='reviews-value reviews' name='value' value=''>value: ${review.value}</span>
+            </div>
+            <div>
+              <span class='reviews-quality reviews' name='quality' value=''>quality: ${review.quality}</span>
+            </div>
+            <div>
+              <span class='reviews-userName reviews' name='userName' value=''>userName: ${review.userName}</span>
+            </div>
+            <div>
+              <span class='reviews-date reviews' name='date' value=''>date: ${review.date}</span>
+            </div>
+            <div>
+              <span class='reviews-foodTruck reviews' name='foodTruck' value=''>Name of Food Truck (id): ${review.foodTruck}</span>
+            </div>
+            <div>
+              <span class='reviews-titleOfReview reviews' name='titleOfReview' value=''>Title Of Review: ${review.titleOfReview}</span>
+            </div>
+            <div>
+              <span class='reviews-content reviews' name='content' value=''>Content: ${review.content}</span>
+            </div>
+            <div>
+              <span class='reviews-image reviews' name='image' value=''>${imgHTML}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class='btn btn-danger red delete-review'>Delete Review</button>
+    </div>
+    `);
+
+  $('#adding-review').append(reviewHTML)
+
+
+
+};
+
+function deleteReview() {
+  console.log('deleteReview is working')
+  let reviewId
+  reviewId = $(this).closest('.review-information').attr('data-review-id');
+  console.log('deleteReview ID', reviewId)
+  var removeReview = {
+  	markedForDeletion: true,
+  }
+  $.ajax ({
+      method: 'DELETE',
+      url: '/api/' + reviewId + '/reviews',
+     	data: removeReview,
+  })
+  .then(function (reviewRemoved) {
+  	console.log('This truck has been removed');
+    console.log($('[data-review-id =' + reviewId + ']').remove())
+    $('[data-review-id =' + reviewId + ']').remove();
+  });
+};
+
+function writeReviewInputOpen() {
+  $('.write-review-information').remove();
+  console.log('writeReview is working');
+  let foodTruckIdForWriteReview
+  foodTruckIdForWriteReview = $(this).closest('.card-truck').attr('data-truck-id');
+  console.log('foodTruckIdForWriteReview ID', foodTruckIdForWriteReview)
+
+  let writeReviewHtml = ( `
+    <div class="write-review-information" data-write-foodtruck-id='${foodTruckIdForWriteReview}'>
+      <div class="row reviews">
+        <div class="col s12 review-information-info">
+          <input class='input-for-reviews reviews-atmosphere-input' id='input-atmosphere' placeholder="Food Truck Atmosphere"></input>
+          <input class='input-for-reviews reviews-value-input' id='input-value' placeholder="Food Truck Value"></input>
+          <input class='input-for-reviews reviews-quality-input' id='input-quality' placeholder="Food Truck Quality"></input>
+          <input class='input-for-reviews reviews-titleOfReview-input' id='input-titleOfReview' placeholder="Title Of Review"></input>
+          <input class='input-for-reviews reviews-content-input' id='input-content' placeholder="Content of Review"></input>
+          <input class='input-for-reviews reviews-userName-input' id='input-content' placeholder="Username Here"></input>
+
+
+            <div class="file-field input-field">
+              <div class="btn">
+                <span>File</span>
+                <input type="file" multiple>
+              </div>
+              <div class="file-path-wrapper">
+                <input class='file-path validate input-for-file' name='image' id='input-review-images' type="text" placeholder="Upload your food images">
+              </div>
+            </div>
+          <button class='btn blue write-truck-review-submit review-buttons'>Submit Review</button>
+        </div>
+      </div>
+    </div>
+  `);
+  $('#write-review').append(writeReviewHtml)
+}
+
+function writeReviewSubmit () {
+  console.log('writeReviewSubmit is working');
+  let foodTruckIdForWhenSubmittingReview
+  foodTruckIdForWhenSubmittingReview = $(this).closest('.write-review-information').attr('data-write-foodtruck-id');
+
+  console.log('foodTruckIdForWhenSubmittingReview ID', foodTruckIdForWhenSubmittingReview)
+  let writeReviewAtmosphere = $('.reviews-atmosphere-input').val();
+  let writeReviewValue = $('.reviews-value-input').val();
+  let writeReviewQuality = $('.reviews-quality-input').val();
+  let writeReviewTitleOfReview = $('.reviews-titleOfReview-input').val();
+  let writeReviewContent = $('.reviews-content-input').val();
+  let writeReviewUserName = $('.reviews-userName-input').val();
+
+// need to fix the way to parse images
+  let writeReviewImages = [];
+  // remove the spaces first
+  let writeReviewImagesUploaded = $('.input-for-file').val().replace(/\s/g, '').split(',');
+
+  let filePath = '/images/food_truck/reviews/';
+  for ( let i = 0; i < writeReviewImagesUploaded.length; i++) {
+    writeReviewImages.push(filePath.concat(writeReviewImagesUploaded[i]));
+  };
+  console.log(writeReviewImagesUploaded)
+
+
+  let writeReviewData = {
+    atmosphere: writeReviewAtmosphere,
+    value: writeReviewValue,
+    quality: writeReviewQuality,
+    titleOfReview: writeReviewTitleOfReview,
+    content: writeReviewContent,
+    userName: writeReviewUserName,
+    image: writeReviewImages,
+    foodTruck: foodTruckIdForWhenSubmittingReview,
+  };
+  $.ajax ({
+      method: 'POST',
+      url: '/api/' + foodTruckIdForWhenSubmittingReview + '/reviews',
+     	data: writeReviewData,
+  })
+  .then(function (createdReview) {
+    $('.write-review-information').remove();
+    renderReview(createdReview)
+  	console.log('This is the new review that was created ', writeReviewData);
+  });
+};
